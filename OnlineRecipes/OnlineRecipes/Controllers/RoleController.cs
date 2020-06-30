8b6aae53a4a59fe973058c0e2c99d7cc3dc2b952
jakub <jakubstankowski@protonmail.com>
2020-06-30 20:35:14 +0200

    Implement role controller and views

---
+++ OnlineRecipes/OnlineRecipes/Controllers/RoleController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using OnlineRecipes.Models;
+
+namespace OnlineRecipes.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            var roles = db.Roles.ToList();
+            return View(roles);
+        }
+
+        // GET: Role/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    db.Roles.Add(role);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                }
+            catch(Exception e)
+            {
+                throw e;
+            }
+
+            return View();
+        }
+
+        // GET: Role/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
